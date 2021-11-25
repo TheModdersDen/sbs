2924c416be3997fcb3d2963d47e4137c96752654
TheModdersDen <monsterspawned@gmail.com>
2021-11-25 12:48:24 -0700

    Fixed a couple of things...

--- sbs.py
+++ sbs.py
@@ -15,33 +15,34 @@ from elevate import elevate
 from sbs_vars import SBS_vars
 from utils import Utils
 
+
 class SBS():
-    
-    feed = "https://reddit.com/r/showerthoughts/top.rss?t="
+
     vars = SBS_vars()
-    env_vars_dict = {}
-    vars_list = {}
-    utils = Utils()
-    
-    thought_file = f"thoughts/thoughts-{vars.currentDate}--{vars.current_time}.txt"
-    thoughts_data = None
-    def is_root():
-        return os.getuid() == 0
-    
-    if is_root() == False:
-        elevate()
-    else:
-        utils.LOG_DEBUG("Process is running as administrator/root. No need to elevate.")
-    
-    try:
-            main_url = config("MAIN_URL")
-            out_dir = config("OUT_DIR")
-            feed_dir = config("FEED_DIR")
-            update_frequency = config("REFRESH_RATE")
-    except {Exception} as error:
-            print(f"ERROR: {error}")
-            utils.LOG_DEBUG(f"ERROR: {error}")
-    
+
+    def __init__(self):
+        self.feed = "https://reddit.com/r/showerthoughts/top.rss?t="
+        self.env_vars_dict = {}
+        self.vars_list = {}
+        utils = Utils()
+
+        self.thought_file = f"thoughts/thoughts-{self.vars.currentDate}--{self.vars.current_time}.txt"
+        self.thoughts_data = None
+
+        def is_root():
+            return os.getuid() == 0
+
+        if is_root() == False:
+            elevate()
+        else:
+            utils.LOG_DEBUG(
+                "Process is running as administrator/root. No need to elevate.")
+
+            self.main_url = self.vars.main_url
+            self.out_dir = self.vars.out_dir
+            self.feed_dir = self.vars.feed_dir
+            self.update_frequency = self.vars.update_frequency
+
     def isProfane(self, input):
         if predict_prob([input]) >= [0.1] or predict([input]) == [1]:
             return True
@@ -66,23 +67,28 @@ class SBS():
         for post in d.entries:
             if self.isProfane(post.title):
                 print(f"NOT ADDING: '{post.title}' as it is profane.")
-                self.utils.LOG_INFO(f"NOT ADDING: '{post.title}' as it is profane.")
+                self.vars.utils.LOG_INFO(
+                    f"NOT ADDING: '{post.title}' as it is profane.")
             else:
                 post_index += 1
                 print(f"ADDING: '{post.title}' as it is NOT profane.")
-                self.utils.LOG_INFO(f"ADDING: '{post.title}' as it is NOT profane.")
-                self.create_tts_file(post.title, os.getcwd(), f"polly_out{post_index}.mp3")
-        thoughts = ['ShowerThoughts Database File:', f"{self.vars.currentDate}--{self.vars.current_time}"]
+                self.vars.utils.LOG_INFO(
+                    f"ADDING: '{post.title}' as it is NOT profane.")
+                self.create_tts_file(
+                    post.title, os.getcwd(), f"polly_out{post_index}.mp3")
+        thoughts = ['ShowerThoughts Database File:',
+                    f"{self.vars.currentDate}--{self.vars.current_time}"]
         for post in d.entries:
             thoughts.append(post.title)
-        thoughts.append("That's all for today! Come back tomorrow for more ShowerThoughts.")
-        self.utils.create_file_from_path(f"{os.getcwd()}/thoughts/")
+        thoughts.append(
+            "That's all for today! Come back tomorrow for more ShowerThoughts.")
+        self.vars.utils.create_file_from_path(f"{os.getcwd()}/thoughts/")
         with open(self.thought_file, "w+") as thought_file:
             for thought in thoughts:
                 thought_file.write(thought + "\n")
 
-    def create_tts_file(self, data, out_dir_var,  out_file_name):        
-        polly_command = f'cd {out_dir_var} && sudo aws polly synthesize-speech --text-type ssml --output-format "mp3" --voice-id "Salli" --engine neural --text "<speak>{data}</speak>" {out_file_name}'        
+    def create_tts_file(self, data, out_dir_var,  out_file_name):
+        polly_command = f'cd {out_dir_var} && sudo aws polly synthesize-speech --text-type ssml --output-format "mp3" --voice-id "Salli" --engine neural --text "<speak>{data}</speak>" {out_file_name}'
         print(polly_command)
         os.system(polly_command)
 
@@ -95,7 +101,7 @@ class SBS():
         file_names = file_names[1:]
         merge_command = f"cat {file_names} >polly_output{self.vars.currentId}.mp3"
         os.system(merge_command)
-        self.utils.LOG_DEBUG("Running command: '"+ merge_command + "'")
+        self.vars.utils.LOG_DEBUG("Running command: '" + merge_command + "'")
         file_names = ""
         for tts_file in tts_files:
             if tts_file == "_intro.mp3" or tts_file == "z_outro.mp3":
@@ -104,10 +110,10 @@ class SBS():
                 file_names += f" {tts_file}"
         cleanup_command = f"rm {file_names}"
         os.system(cleanup_command)
-        self.utils.LOG_DEBUG("Running command: '"+ cleanup_command + "'")
+        self.vars.utils.LOG_DEBUG("Running command: '" + cleanup_command + "'")
 
         move_command = f"mv polly_output{self.vars.currentId}.mp3 {self.vars.out_dir}"
-        self.utils.LOG_DEBUG("Running command: '"+ move_command + "'")
+        self.vars.utils.LOG_DEBUG("Running command: '" + move_command + "'")
         os.system(move_command)
 
     def create_rss_feed(self):
@@ -118,7 +124,8 @@ class SBS():
             ttl=60,
             language="en")
 
-        tts_filesize = self.utils.get_file_size(self.vars.out_dir + self.vars.out_file)
+        tts_filesize = self.vars.utils.get_file_size(
+            self.vars.out_dir + self.vars.out_file)
 
         test_feed.add_item(
             unique_id=self.vars.URN_UUID,
@@ -136,25 +143,25 @@ class SBS():
             rss_feed.close()
 
     def main(self):
-        version_URL = urllib.request.urlopen(f"{self.main_url}ST_VERSION")
-        self.utils.LOG_DEBUG(
+        version_URL = urllib.request.urlopen(f"{self.vars.main_url}ST_VERSION")
+        self.vars.utils.LOG_DEBUG(
             f"result code for version_URL: {str(version_URL.getcode())}".strip("\n"))
         data = version_URL.read()
         print(f"Starting SBS v{data}... Please wait.")
         currentOS = self.getCurrentOS()
-        self.utils.LOG_INFO(f"Current OS type is: '{platform.system()}.'")
-        self.vars.createEnvVars()
+        self.vars.utils.LOG_INFO(f"Current OS type is: '{platform.system()}.'")
         if currentOS == 0:
-            self.utils.create_file_from_path("/var/www/html/st/polly_out/")
+            self.vars.utils.create_file_from_path("/var/www/html/st/polly_out/")
         elif currentOS == 1:
             print("Your OS, Windows, is currently not supported. Exiting gracefully...")
-            self.utils.LOG_ERROR(
+            self.vars.utils.LOG_ERROR(
                 "Your OS, Windows, is currently not supported. Exiting gracefully...")
             sleep(5)
             exit(1)
         elif currentOS == 2:
-            print("Your OS, Darwin (AKA: macOS or Mac OS X), is currently not supported. Exiting gracefully...")
-            self.utils.LOG_ERROR(
+            print(
+                "Your OS, Darwin (AKA: macOS or Mac OS X), is currently not supported. Exiting gracefully...")
+            self.vars.utils.LOG_ERROR(
                 "Your OS, Darwin (AKA: macOS or Mac OS X), is currently not supported. Exiting gracefully...")
             sleep(5)
             exit(1)
@@ -162,6 +169,7 @@ class SBS():
         self.processRedditFeed(self.feed + self.update_frequency)
         self.make_polly_file(os.getcwd())
         self.create_rss_feed()
+
 
 if __name__ == "__main__":
     sbs = SBS()
--- sbs_vars.py
+++ sbs_vars.py
@@ -5,70 +5,70 @@ from uuid import uuid4
 import datetime
 from decouple import config
 
+
 class SBS_vars():
     utils = Utils()
+    
+    def __init__(self):
+        self.createEnvVars()
+        self.main_url = config("TTS_MAIN_URL")
+        self.out_dir = config("TTS_OUT_DIR")
+        self.feed_dir = config("FEED_DIR")
+        self.update_frequency = config("REFRESH_RATE")
 
-    try:
-
-        main_url = config("MAIN_URL")
-        out_dir = config("OUT_DIR")
-        feed_dir = config("FEED_DIR")
-        update_frequency = config("REFRESH_RATE")
-        uuid = uuid4()
-        currentId = str(uuid)
-        URN_UUID = uuid.urn
-        encoding_type = "utf-8"
-        dt = datetime.date.today()
-        now = datetime.datetime.now()
-        current_time = now.strftime("%H:%M:%S")
-        currentDate = dt.isoformat()
-        utils.LOG_DEBUG(f"The current date and time is: '{currentDate}--{current_time}.'")
-        out_file = f"polly_output{currentId}.mp3"
-        feed_title = f"Your ShowerThoughts Update ({currentDate}--{current_time})"
-        feed_link = main_url + f"polly_out/polly_output{currentId}.mp3"
-        feed_description = "This is an expiremental feed to use Amazon Polly to read the ShowerThoughts Reddit page to end users."
-    except {Exception} as error:
-        print(f"ERROR: {error}")
-        utils.LOG_DEBUG(f"ERROR: {error}")
+        self.uuid = uuid4()
+        self.currentId = str(self.uuid)
+        self.URN_UUID = self.uuid.urn
+        self.encoding_type = "utf-8"
+        self.dt = datetime.date.today()
+        self.now = datetime.datetime.now()
+        self.current_time = self.now.strftime("%H:%M:%S")
+        self.currentDate = self.dt.isoformat()
+        self.utils.LOG_DEBUG(
+            f"The current date and time is: '{self.currentDate}--{self.current_time}.'")
+        self.out_file = f"polly_output{self.currentId}.mp3"
+        self.feed_title = f"Your ShowerThoughts Update ({self.currentDate}--{self.current_time})"
+        self.feed_link = self.main_url + f"polly_out/polly_output{self.currentId}.mp3"
+        self.feed_description = "This is an expiremental feed to use Amazon Polly to read the ShowerThoughts Reddit page to end users."
 
     def createEnvVars(self):
         if os.path.isfile(f"{os.getcwd()}/.env") == False or self.utils.is_file_empty(f"{os.getcwd()}/.env"):
-            env_file = open(f"{os.getcwd()}/.env", "w+")
-            url_input = str(input(
+            env_file=open(f"{os.getcwd()}/.env", "w+")
+            url_input=str(input(
                 "What is the main URL which will be read by the Alexa Skill? (A full URL w/o quotes): "))
             if url_input == None:
                 raise Exception(
-                    "Please put a valid URL as the MAIN_URL input.\nRun this program to try again.")
+                    "Please put a valid URL as the 'TTS_MAIN_URL' input.\nRun this program to try again.")
             else:
                 self.utils.LOG_DEBUG(
-                    f"Writing '{url_input}' as the MAIN_URL variable.")
-                env_file.write(f"MAIN_URL={url_input}\n")
-            output_dir = str(input(
+                    f"Writing '{url_input}' as the 'TTS_MAIN_URL' variable.")
+                env_file.write(f"TTS_MAIN_URL={url_input}\n")
+            output_dir=str(input(
                 "Where would you like the final output of the ShowerThoughts text-to-speech '.mp3' file to be stored? (example: /var/www/html/): "))
             if output_dir == None:
                 raise Exception(
-                    "Please make that the 'output_dir' input is a string/path.\nRun this program to try again.")
+                    "Please make that the 'TTS_OUT_DIR' input is a string/path.\nRun this program to try again.")
             else:
                 self.utils.LOG_DEBUG(
-                    f"Writing '{output_dir}' as the OUT_DIR variable.")
-                env_file.write(f"OUT_DIR={output_dir}\n")
-            feed_dir = str(input(
+                    f"Writing '{output_dir}' as the 'TTS_OUT_DIR' variable.")
+                env_file.write(f"TTS_OUT_DIR={output_dir}\n")
+            feed_dir=str(input(
                 "Where would you like the final output of the ShowerThoughts RSS '.xml' feed file to be stored? (example: /var/www/html/): "))
             if feed_dir == None:
                 raise Exception(
-                    "Please make that the 'feed_dir' input is a string/path.\nRun this program to try again.")
+                    "Please make that the 'FEED_DIR' input is a string/path.\nRun this program to try again.")
             else:
                 self.utils.LOG_DEBUG(
-                    f"Writing '{feed_dir}' as the OUT_DIR variable.")
+                    f"Writing '{feed_dir}' as the 'FEED_DIR' variable.")
                 env_file.write(f"FEED_DIR={feed_dir}\n")
-            refresh_rate = str(input(
+            refresh_rate=str(input(
                 "How often would you like the feed to be updated? NOTE: The more the feed is updated, the more strain will be put on your system (day/hour): "))
             if refresh_rate not in ["hour", "day"]:
                 raise Exception(
-                    "Please make that the 'refresh_rate' input is set to either 'day' or 'hour'.\nRun this program to try again.")
+                    "Please make that the 'REFRESH_RATE' input is set to either 'day' or 'hour'.\nRun this program to try again.")
             else:
                 self.utils.LOG_DEBUG(
-                    f"Writing '{refresh_rate}' as the OUT_DIR variable.")
+                    f"Writing '{refresh_rate}' as the 'REFRESH_RATE' variable.")
                 env_file.write(f"REFRESH_RATE={refresh_rate}\n")
             env_file.close()
             self.utils.LOG_DEBUG("Done writing program environment variables.")
