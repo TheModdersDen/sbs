15fda7d07870b080f3be01c5df1ac06e54d0505c
TheModdersDen <monsterspawned@gmail.com>
2021-12-08 09:42:50 -0700

    Updated test.py

---
+++ test.py
@@ -0,0 +1,15 @@
+from utils import Utils
+from sbs_vars import SBS_vars
+
+vars = SBS_vars()
+utils = Utils()
+
+distro_name = vars.distro
+            distro_ver = vars.distro_version
+            utils.LOG_DEBUG(f"You are running {distro_name} v{distro_ver}.")
+            if distro_name in ["Ubuntu".lower(), "Debian".lower(), "Kali".lower(), "raspbian".lower()]:  
+                utils.create_file_from_path(
+                    vars.out_dir
+                )
+            else:
+                utils.LOG_ERROR("You are running on an unsupported Linux Distro. Please install either Debian or Ubuntu and try again.")
