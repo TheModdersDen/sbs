dbb6f5a1455c0cb1e3014f8898b872942af482dc
TheModdersDen <monsterspawned@gmail.com>
2023-06-15 17:02:32 -0600

    Moved net functions into proper class.

--- utils/net_utils.py
+++ utils/net_utils.py
@@ -22,19 +22,21 @@
 #! A class to handle network functions for the ShowerThoughts Briefing Skill
 
 # Imports
-import socket
-from requests import get
 
 from sbs import SBS
 
-from datetime import datetime, timedelta    
-from time import sleep, time, strftime, gmtime
+from datetime import datetime, timedelta
+
+from os.path import exists, isfile, isdir, join
+
+from utils.os_utils import OSUtils
 
 class NetworkUtils():
     
     def __main__(self):
         self.sbs = SBS()
         self.logger = self.sbs.logger
+        self.os_utils = OSUtils()
         
         self.logger.debug('Network Utils initializing...')
     
@@ -96,6 +98,200 @@ class NetworkUtils():
             self.logger.error(f'Could not download file from {url}: {e}')
             return False
     
-    # Get the external IP address of the device
-    def get_device_ip_address(self) -> str:
-        return get('https://api.ipify.org').decode('utf8')
+    # Get the external IP address of the machine or network:
+
+    def get_external_ip_address(self) -> str:
+        from requests import get
+        try:
+            self.sbs.logger.debug("Getting external IP address...")
+            return get('https://api.ipify.org').content.decode('utf8')
+        except Exception as e:
+            self.sbs.logger.error("An error occurred when trying to resolve an IP address. Error Info:\n"+ e)
+            return None
+        
+        # Get the internal IP address of the machine or network:
+    def get_internal_ip_address(self) -> str:
+        try:
+            import socket
+            self.sbs.logger.debug("Getting internal IP address...")
+            return socket.gethostbyname(socket.gethostname())
+        except Exception as e:
+            self.sbs.logger.error("An error occurred when trying to resolve an IP address. Error Info:\n"+ e)
+            return None
+    
+        # Get a list of open ports on the machine or network:
+    def get_open_ports(self) -> list:
+        try:
+            import subprocess
+            
+            self.sbs.debug("Getting open ports...")
+            
+            open_ports = []
+            if self._os != 'Linux':
+                raise NotImplementedError
+            
+            subprocess.call('netstat -an > netstat.txt', shell=True)
+            
+            with open('netstat.txt', 'r') as netstat:
+                for line in netstat:
+                    if 'LISTEN' in line:
+                        open_ports.append(line.split()[3].split(':')[1])
+                    else:
+                        continue
+            return open_ports
+        except Exception as e:
+            self.sbs.logger.error("An error occurred when trying to resolve open ports. Error Info:\n"+ e)
+            return None
+    
+    # Get the current webserver in use on the machine:
+    def get_webserver(self) -> str:
+        if self._os == 'Windows':
+            return None
+        elif self._os == 'Linux':
+            if exists("/etc/apache2"):
+                return "Apache"
+            elif exists("/etc/nginx"):
+                return "Nginx"
+            elif exists("/etc/lighttpd"):
+                return "Lighttpd"
+            elif exists("/etc/httpd"):
+                return "Httpd"
+            elif exists("/etc/cherokee"):
+                return "Cherokee"
+            elif exists("/etc/varnish"):
+                return "Varnish"
+            elif exists("/etc/squid"):
+                return "Squid"
+            elif exists("/etc/pound"):
+                return "Pound"
+            elif exists("/etc/hiawatha"):
+                return "Hiawatha"
+            elif exists("/etc/haproxy"):
+                return "HAProxy"
+            elif exists("/etc/stunnel"):
+                return "Stunnel"
+            elif exists("/etc/monkey"):
+                return "Monkey"
+            elif exists("/etc/mini_httpd"):
+                return "Mini_httpd"
+            elif exists("/etc/mongoose"):
+                return "Mongoose"
+        elif self._os == 'Darwin':
+            return None
+        pass
+
+    # From the webserver, get the version of the webserver:
+    def get_webserver_version(self, webserver = "") -> str:
+        if webserver == "" or webserver == None:
+            webserver = self._webserver_name
+        elif webserver == "Apache":
+            with open("/etc/apache2/httpd.conf", "r") as httpd_conf:
+                for line in httpd_conf:
+                    if "ServerTokens" in line:
+                        return line.split()[1]
+        elif webserver == "Nginx":
+            with open("/etc/nginx/nginx.conf", "r") as nginx_conf:
+                for line in nginx_conf:
+                    if "nginx/" in line:
+                        return line.split()[1]
+        elif webserver == "Lighttpd":
+            with open("/etc/lighttpd/lighttpd.conf", "r") as lighttpd_conf:
+                for line in lighttpd_conf:
+                    if "lighttpd/" in line:
+                        return line.split()[1]
+        elif webserver == "Httpd":
+            with open("/etc/httpd/httpd.conf", "r") as httpd_conf:
+                for line in httpd_conf:
+                    if "ServerTokens" in line:
+                        return line.split()[1]
+        elif webserver == "Cherokee":
+            with open("/etc/cherokee/cherokee.conf", "r") as cherokee_conf:
+                for line in cherokee_conf:
+                    if "ServerTokens" in line:
+                        return line.split()[1]    
+        elif webserver == "Varnish":
+            with open("/etc/varnish/varnish.conf", "r") as varnish_conf:
+                for line in varnish_conf:
+                    if "ServerTokens" in line:
+                        return line.split()[1]
+        elif webserver == "Squid":
+            with open("/etc/squid/squid.conf", "r") as squid_conf:
+                for line in squid_conf:
+                    if "ServerTokens" in line:
+                        return line.split()[1]
+        elif webserver == "Pound":
+            with open("/etc/pound/pound.cfg", "r") as pound_conf:
+                for line in pound_conf:
+                    if "ServerTokens" in line:
+                        return line.split()[1]
+        elif webserver == "Hiawatha":
+            with open("/etc/hiawatha/hiawatha.conf", "r") as hiawatha_conf:
+                for line in hiawatha_conf:
+                    if "ServerTokens" in line:
+                        return line.split()[1]
+        elif webserver == "HAProxy":
+            with open("/etc/haproxy/haproxy.cfg", "r") as haproxy_conf:
+                for line in haproxy_conf:
+                    if "ServerTokens" in line:
+                        return line.split()[1]
+        elif webserver == "Stunnel":
+            with open("/etc/stunnel/stunnel.conf", "r") as stunnel_conf:
+                for line in stunnel_conf:
+                    if "ServerTokens" in line:
+                        return line.split()[1]
+        elif webserver == "Monkey":
+            with open("/etc/monkey/monkey.conf", "r") as monkey_conf:
+                for line in monkey_conf:
+                    if "ServerTokens" in line:
+                        return line.split()[1]
+        elif webserver == "Mini_httpd":
+            with open("/etc/mini_httpd/mini_httpd.conf", "r") as mini_httpd_conf:
+                for line in mini_httpd_conf:
+                    if "ServerTokens" in line:
+                        return line.split()[1]
+        elif webserver == "Mongoose":
+            with open("/etc/mongoose/mongoose.conf", "r") as mongoose_conf:
+                for line in mongoose_conf:
+                    if "ServerTokens" in line:
+                        return line.split()[1]
+        else:
+            return None #! Unknown webserver
+
+    # Get the port the webserver is running on:
+    def get_webserver_port(self, webserver = "") -> int:
+        if webserver == "" or webserver == None:
+            webserver = self._webserver_name
+        elif webserver == "Apache":
+            return 80
+        elif webserver == "Nginx":
+            return 80
+        elif webserver == "Lighttpd":
+            return 80
+        elif webserver == "Httpd":
+            return 80
+        elif webserver == "Cherokee":
+            return 80
+        elif webserver == "Varnish":
+            return 80
+        elif webserver == "Squid":
+            return 80
+        elif webserver == "Pound":
+            return 80
+        elif webserver == "Hiawatha":
+            return 80
+        elif webserver == "HAProxy":
+            return 80
+        elif webserver == "Stunnel":
+            return 80
+        elif webserver == "Monkey":
+            return 80
+        elif webserver == "Mini_httpd":
+            return 80
+        elif webserver == "Mongoose":
+            return 80
+        else:
+            return -1 #! Unknown webserver
+        
+    # Get the hostname of the machine:
+    def get_hostname(self) -> str:
+        return self.os_utils._node
